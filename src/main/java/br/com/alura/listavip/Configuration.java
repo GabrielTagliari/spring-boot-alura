76e275a8230c8c6100e88b8352abea0c39c1492f
Gabriel Tagliari Rodrigo <gabriel.tagliari93@gmail.com>
2017-11-03 23:37:40 -0200

    Criação da lista de convidados e ConvidadoRepository

---
+++ src/main/java/br/com/alura/listavip/Configuration.java
@@ -0,0 +1,26 @@
+package br.com.alura.listavip;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@SpringBootApplication(scanBasePackages = {"br.com.alura"})
+public class Configuration {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Configuration.class, args);
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource datasource = new DriverManagerDataSource();
+		datasource.setDriverClassName("com.mysql.jdbc.Driver");
+		datasource.setUrl("jdbc:mysql://localhost:3306/listavip");
+		datasource.setUsername("root");
+		datasource.setPassword("root");
+		return datasource;
+	}
+}
--- src/main/java/br/com/alura/listavip/controller/ConvidadoController.java
+++ src/main/java/br/com/alura/listavip/controller/ConvidadoController.java
@@ -1,14 +1,29 @@
 package br.com.alura.listavip.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.alura.listavip.model.Convidado;
+import br.com.alura.listavip.repository.ConvidadoRepository;
 
 @Controller
 public class ConvidadoController {
 	
+	@Autowired
+	private ConvidadoRepository convidadoRepository;
+
 	@RequestMapping("/")
 	public String index() {
 		System.out.println("Entrei no controller");
 		return "index";
+	}
+	
+	@RequestMapping("/listaconvidados")
+	public String lista(Model model) {
+		Iterable<Convidado> convidados = convidadoRepository.findAll();
+		model.addAttribute("convidados", convidados);
+		return "listaconvidados";
 	}
 }
